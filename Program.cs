7a147f01433a037584c61294aa278894d0e389ac
Vetrov Yury <888vetrov@gmail.com>
2022-04-10 20:28:53 +0300

    1 task

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, 
+// какое число больше, а какое меньше
+// 1) a=5; b=7 -> max = 7
+// 2) a=2; b=10 -> max = 10
+// 3) a=-9; b=-3 -> max = -3
+int numberA, numberB;
+numberA = -9;
+numberB = -3;
+
+if (numberA > numberB)
+{
+    System.Console.WriteLine(numberA);
+}
+else 
+System.Console.WriteLine(numberB);
